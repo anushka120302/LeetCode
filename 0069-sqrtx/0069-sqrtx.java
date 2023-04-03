429773b5f33260812dfa2cd5955a489920a780a4
Anushka Agarwal <87578160+anushka120302@users.noreply.github.com>
2023-04-03 13:50:41 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (12.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,31 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        //for(int i =1;i<=x;i++)
+        // {
+        //     if(i*i>x)
+        //         return i-1;
+        // }
+        // complexity: root of log(n)
+        
+        if(x<0)
+            return -1;
+        if(x==0)
+            return 0;
+        if(x<4)
+            return 1;
+        
+        int low=1, high =x;
+        
+        while(low<high)
+        {
+            int mid =(low+high)/2;
+            if(x/mid==mid)
+                return mid;
+            else if(x/mid>mid)
+                low=mid+1;
+            else
+                high=mid;
+        }
+        return low-1;
     }
-}
+}   
