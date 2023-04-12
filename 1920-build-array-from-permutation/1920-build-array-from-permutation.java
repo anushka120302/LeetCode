3805ecd4bc084e0a7b68ceb6e73fbae71dba836e
Anushka Agarwal <87578160+anushka120302@users.noreply.github.com>
2023-04-12 09:41:42 +0530

    Time: 1 ms (97.61%), Space: 43.1 MB (71.73%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public int[] buildArray(int[] nums) 
+    {
+        int ar[]=new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            ar[i]=nums[nums[i]];
+        }
+        
+        return ar;
+    }
+}
